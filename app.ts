86b3452e7cf2c45d4c3ea53ffc4a9410be8a1660
lovesmallpaes <mike.huang.mikank@gmail.com>
2023-04-30 15:15:26 +0100

    Added typeorm and configuration, cors, ans bodyparser

--- app.ts
+++ app.ts
@@ -3,15 +3,37 @@ import * as dotenv from 'dotenv';
 if (process.env.NODE_ENV !== 'production') { 
   dotenv.config();
 }
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { DataSource } from "typeorm";
 
 const app: Express = express();
-
 const port = process.env.PORT || 3000;
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: true,
+});
+
+// parse request body json and attach to req.body
+// as javascript object
+app.use(bodyParser.json());
+
+// use cors
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`)
-})
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data source initialized")
+    app.listen(port, () => console.log(`App is running on port ${port}`))
+  })
+  .catch(e => console.error("Error initializing data source: ", e))
